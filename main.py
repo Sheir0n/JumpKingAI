e171163c6088d6e47d5e87712eef6baac151891a
gajpawel <pg305038@student.polsl.pl>
2025-04-12 20:21:07 +0200

    Prosta plansza

--- main.py
+++ main.py
@@ -9,16 +9,35 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Moje Okno")
 
+player = pygame.Rect((400, 550, 25, 25))
+ground = pygame.Rect((0, 575, 800, 25))
+
 # Główna pętla gry
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (250, 0, 0), player)
+    pygame.draw.rect(screen, (0, 200, 100), ground)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Odświeżanie ekranu (np. na czarno)
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+    # Ograniczenie ruchu do granic ekranu
+    if player.left < 0:
+        player.left = 0
+    if player.right > SCREEN_WIDTH:
+        player.right = SCREEN_WIDTH
+
+    pygame.display.update()
 
 # Zakończenie
 pygame.quit()
